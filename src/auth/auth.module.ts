fd42aff3cceefedaf4ea5dc48e2db716aadb48b9
João Jeefer da Silva Félix <ter_jfelix@arezzo.com.br>
2024-09-15 17:51:04 -0300

    feat: :wrench: add config module to handle jwt token key

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthController],
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      global: true,
+      secret: String(process.env.JWT_SECRET),
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   providers: [AuthService],
 })
 export class AuthModule {}
