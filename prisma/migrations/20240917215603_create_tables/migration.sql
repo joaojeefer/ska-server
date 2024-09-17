1612eb95cbf4dbaf6094396a1b4c2952f9258195
João Jeefer da Silva Félix <ter_jfelix@arezzo.com.br>
2024-09-17 19:01:53 -0300

    refactor: :recycle: update database schema

--- prisma/migrations/20240917215603_create_tables/migration.sql
+++ prisma/migrations/20240917215603_create_tables/migration.sql
@@ -40,6 +40,31 @@ CREATE TABLE "Usuario" (
     CONSTRAINT "Usuario_turno_id_fkey" FOREIGN KEY ("turno_id") REFERENCES "Turno" ("turno_id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
+-- CreateTable
+CREATE TABLE "Maquina" (
+    "maquina_id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "descricao" TEXT NOT NULL,
+    "localizacao" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Indicadores" (
+    "indicadores_id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "ind_oee" REAL NOT NULL,
+    "ind_qualidade" REAL NOT NULL,
+    "ind_performance" REAL NOT NULL,
+    "ind_disponibilidade" REAL NOT NULL,
+    "tempo_programado" INTEGER NOT NULL,
+    "tempo_em_producao" INTEGER NOT NULL,
+    "tempo_teorico_producao" INTEGER NOT NULL,
+    "tempo_paradas" INTEGER NOT NULL,
+    "pecas_produzidas" INTEGER NOT NULL,
+    "pecas_defeito" INTEGER NOT NULL,
+    "data" DATETIME NOT NULL,
+    "maquina_id" INTEGER NOT NULL,
+    CONSTRAINT "Indicadores_maquina_id_fkey" FOREIGN KEY ("maquina_id") REFERENCES "Maquina" ("maquina_id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "Contato_email_key" ON "Contato"("email");
 
@@ -50,7 +75,4 @@ CREATE UNIQUE INDEX "Contato_usuario_id_key" ON "Contato"("usuario_id");
 CREATE UNIQUE INDEX "Usuario_cpf_key" ON "Usuario"("cpf");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Usuario_tipo_usuario_id_key" ON "Usuario"("tipo_usuario_id");
-
--- CreateIndex
-CREATE UNIQUE INDEX "Usuario_turno_id_key" ON "Usuario"("turno_id");
+CREATE UNIQUE INDEX "Indicadores_maquina_id_key" ON "Indicadores"("maquina_id");
