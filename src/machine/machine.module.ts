d5cb6baa9a1e8c7308c5edd6d4d40c3f4a9059c1
João Jeefer da Silva Félix <ter_jfelix@arezzo.com.br>
2024-09-17 19:44:01 -0300

    refactor: :sparkles: add db-module to machine and metrics modules

--- src/machine/machine.module.ts
+++ src/machine/machine.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MachineController } from './machine.controller';
 import { MachineService } from './machine.service';
+import { DbModule } from 'src/db/db.module';
 
 @Module({
   controllers: [MachineController],
+  imports: [DbModule],
   providers: [MachineService],
 })
 export class MachineModule {}
