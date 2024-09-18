6cf48e0fc35a7ef8734c6ee3eddb67f78fb96cfa
João Jeefer da Silva Félix <ter_jfelix@arezzo.com.br>
2024-09-18 19:11:47 -0300

    refactor: :fire: remove role and shift requests from user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,18 +1,10 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
-import CreateUserDTO from './dto/user';
-import { RoleService } from 'src/role/role.service';
-import CreateRoleDTO from 'src/role/dto/role';
-import { ShiftService } from 'src/shift/shift.service';
-import CreateShiftDTO from 'src/shift/dto/shift';
+import CreateUserDTO from './dto/create-user.dto';
 
 @Controller('users')
 export class UsersController {
-  constructor(
-    private roleService: RoleService,
-    private shiftService: ShiftService,
-    private usersService: UsersService,
-  ) {}
+  constructor(private usersService: UsersService) {}
 
   @Get()
   async getUsers() {
@@ -27,27 +19,5 @@ export class UsersController {
   @Post()
   async createUser(@Body() user: CreateUserDTO) {
     return this.usersService.createUser(user);
-  }
-
-  // Role feature
-  @Get()
-  async getRoles() {
-    return this.roleService.getRoles();
-  }
-
-  @Post()
-  async createRole(@Body() role: CreateRoleDTO) {
-    return this.roleService.createRole(role);
-  }
-
-  // Shift feature
-  @Get()
-  async getShifts() {
-    return this.shiftService.getShifts();
-  }
-
-  @Post()
-  async createShift(@Body() shift: CreateShiftDTO) {
-    return this.shiftService.createShift(shift);
   }
 }
