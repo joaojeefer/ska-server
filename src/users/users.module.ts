d2646cd20253855a751e7ec1793f7ebd69b0d919
João Jeefer da Silva Félix <ter_jfelix@arezzo.com.br>
2024-09-17 22:19:26 -0300

    feat: :sparkles: add role and shift request calls to user controller

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { DbModule } from 'src/db/db.module';
+import { RoleModule } from 'src/role/role.module';
+import { ShiftModule } from 'src/shift/shift.module';
 
 @Module({
   controllers: [UsersController],
   exports: [UsersService],
-  imports: [DbModule],
+  imports: [DbModule, RoleModule, ShiftModule],
   providers: [UsersService],
 })
 export class UsersModule {}
