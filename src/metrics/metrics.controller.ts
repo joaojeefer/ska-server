90efaddc2e2a75913cc73b245dbafeca68cc98f6
João Jeefer da Silva Félix <ter_jfelix@arezzo.com.br>
2024-09-17 22:08:35 -0300

    refactor: :recycle: remove the us of UseGuards decorator

--- src/metrics/metrics.controller.ts
+++ src/metrics/metrics.controller.ts
@@ -1,13 +1,11 @@
-import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { MetricsService } from './metrics.service';
-import { AuthGuard } from 'src/auth/guard/auth.guard';
 
 @Controller('metrics')
 export class MetricsController {
   constructor(private metricsService: MetricsService) {}
 
   @Get(':machineId')
-  @UseGuards(AuthGuard)
   async getMetricsByMachine(@Param('machineId') machineId: string) {
     return this.metricsService.getMetricsByMachine(machineId);
   }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,20 +1,17 @@
-import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import CreateUserDTO from './dto/user';
-import { AuthGuard } from 'src/auth/guard/auth.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Get()
-  @UseGuards(AuthGuard)
   async getUsers() {
     return this.usersService.getUsers();
   }
 
   @Get(':id')
-  @UseGuards(AuthGuard)
   async getUserById(@Param('id') id: string) {
     return this.usersService.getUserById(id);
   }
